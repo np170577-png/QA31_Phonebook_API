2a6249227d3495a2c90646d23ac59f1eb102a883
np170577-png <np170577@gmail.com>
2026-05-08 14:46:10 +0300

    create GetAllContactsTestsOkHttp

---
+++ src/test/java/okhttp/GetAllContactsTestsOkHttp.java
@@ -0,0 +1,56 @@
+package okhttp;
+
+import com.google.gson.Gson;
+import dto.ContactDTO;
+import dto.ErrorDTO;
+import dto.GetAllContactsDTO;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GetAllContactsTestsOkHttp {
+
+    String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwic3ViIjoibnA3Nzg4QGluYm94LnJ1IiwiaXNzIjoiUmVndWxhaXQiLCJleHAiOjE3Nzg4MzY3NjAsImlhdCI6MTc3ODIzNjc2MH0.OF0xZXLvltN2LYymo1R98S3NFu3bebbxitPzPFiDxqw";
+    Gson gson = new Gson();
+    OkHttpClient client = new OkHttpClient();
+
+    @Test
+    public void getAllContactsSuccess() throws IOException {
+
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
+                .get()
+                .addHeader("Authorization",token)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        Assert.assertTrue(response.isSuccessful());
+        Assert.assertEquals(response.code(),200);
+        GetAllContactsDTO contactsDTO = gson.fromJson(response.body().string(), GetAllContactsDTO.class);
+        List<ContactDTO> contacts = contactsDTO.getContacts();
+        for(ContactDTO c:contacts){
+            System.out.println(c.getId());
+            System.out.println(c.getEmail());
+            System.out.println("===================");
+        }
+    }
+
+    @Test
+    public void getAllContactsWrongtoken() throws IOException {
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
+                .get()
+                .addHeader("Authorization","asdfkdsgn")
+                .build();
+        Response response = client.newCall(request).execute();
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(),401);
+        ErrorDTO errorDTO = gson.fromJson(response.body().string(),ErrorDTO.class);
+        Assert.assertEquals(errorDTO.getError(), "Unauthorized");
+    }
+}
